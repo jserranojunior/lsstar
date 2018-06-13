a8dd487294f12f5aa81a61111448b6ac2c210fbd
Jorge Serrano Junior <jorgeserarnojunior@hotmail.com>
2018-06-13 10:39:13 -0300

    New files to commit

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -83,7 +83,12 @@
                     <li id="menu-item-11" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-6">
                             <a href="{{Route('fornecedor.index')}}"><i class="fa fa-handshake-o" aria-hidden="true"></i><span class="menu-ocultar">Fornecedores </span></a>
                     </li>
-                     
+
+                    <li id="menu-item-11" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-6">
+                            <a target="_blank" href="https://drive.google.com/open?id=1Lca0Yfic5SInOM1Jkj4CEq6A4WSU5D9I">
+                            <i class="fa fa-cloud" aria-hidden="true"></i><span class="menu-ocultar">EXEMPLO </span></a>
+                    </li>
+                  
                   </li>
 
    
