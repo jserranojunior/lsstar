b024d14e986597dc1a62dc968461af3f1f593c96
jserranojunior <jorgeserranojunior@hotmail.com>
2018-10-04 11:17:46 -0300

    input create manutencao

--- resources/views/manutencao/create.blade.php
+++ resources/views/manutencao/create.blade.php
@@ -1,37 +1,21 @@
 @extends('layouts.header')
 @section('content')
 
-<style>
-    .table > tbody > tr > td{
-        border: 1px solid rgba(210, 210, 210, 0)! important;
-    }
-    .tr-destaque{
-        background-color:#cdd5db;;
-    }
-    .btn-add-contato{
-      
-        width:5%;
-    }
-    .form-menor{
-        float:left;
-        width:90%;
-    }
-</style>
-
-<form action="{{Route('cliente.store')}}" method="post" class="form-horizontal">
 
 
-    <div class="col-xs-10 col-xs-offset-1">
 
-        <div class="box box-info">
-            <div class="box-head">
-                <h3 class="text-center"> Nova Manutenção </h3>
-            </div>
-            <div class="box-body no-padding">
+<div class="col-xs-8 col-xs-offset-2">
+
+    <div class="box box-info">
+        <div class="box-head">
+            <h3 class="text-center"> Nova Casa </h3>
+        </div>
+        <form action="./cadastrar" method="POST">
+            <div class="box-body ">
                 <table class="table text-center table-condensed">
-                    <tr>
+                    <tr class="bg-warning">
                         <td>
-                            <h4 class="text-primary">Casa</h4>
+                            <h5 class="text-primary">Casa</h5>
                         </td>
                         <td>
                             <select name="casa" class="form-control" id="">
@@ -39,29 +23,84 @@
                             </select></td>
                         </td>
                     </tr>
+                    <tr class="bg-warning">
+                        <td>
+                            <h5 class="text-primary">Data de Garantia</h5>
+                        </td>
+                        <td>
+                            <input type="date" class="form-control" name="data_garantia" id=""></td>
+                   
+                        </td>
+                    </tr>
+                    <tr class="bg-warning">
+                        <td>
+                            <h5 class="text-primary">Proprietário</h5>
+                        </td>
+                        <td>
+                            <select name="proprietario" class="form-control" id="">
+                                <option value="">Nome do proprietario</option>
+                            </select></td>
+                        </td>
+                    </tr>
                     <tr>
                         <td>
-                            <h4 class="text-primary">Tipo de solicitação</h4>
+                            <h5 class="text-primary">Tipo de solicitação</h5>
                         </td>
                         <td>
                             <select name="casa" class="form-control" id="">
                                 <option value="">Estrutural</option>
+                                <option value="">Piso</option>
+                                <option value="">Azuleijos</option>
+                                <option value="">Alvenaria</option>
+                                <option value="">Pintura</option>                                
                             </select></td>
                         </td>
                     </tr>
+                    <tr>
+                        <td>
+                            <h5 class="text-primary">Descrição</h5>
+                        </td>
+                        <td>
+                            <input type="text" class="form-control" name="descricao">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <h5 class="text-primary">Data da Solicitação</h5>
+                        </td>
+                        <td>
+                        <input type="date" class="form-control" name="data_solicitacao" id=""></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <h5 class="text-primary">Data da Visita Tecnica</h5>
+                        </td>
+                        <td>
+                        <input type="date" class="form-control" name="data_visita" id=""></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <h5 class="text-primary">Link das Fotos</h5>
+                        </td>
+                        <td>
+                        <input type="text" class="form-control" name="link_fotos" id=""></td>
+                    </tr>
+  
+                    
+
                 </table>
-                <div class="box-footer">
-                    <div class="col-sm-4">
-                        <input type="submit" class="btn btn-primary" value="CADASTRAR">
-                    </div>
-                </div>
             </div>
-        </div>
-        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-</form>
+            <div class="box-footer">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                <input type="submit" class="text-right btn btn-primary" value="Cadastrar">
+            </div>
+        </form>
+    </div>
+</div>
 
-<div>
 
 
 
-    @endsection
+
+
+@endsection
