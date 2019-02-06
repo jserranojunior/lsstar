37274a852c1d49eb6c7c4baf06dd16a6f49d680f
jserranojunior <jorgeserranojunior@hotmail.com>
2019-02-06 15:00:05 -0200

    jquery

--- resources/views/layouts/headertwo.blade.php
+++ resources/views/layouts/headertwo.blade.php
@@ -14,6 +14,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="shortcut icon" href="{{{ url('/img/lsstar.png') }}}">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     </head>
    <body class="hold-transition skin-blue sidebar-mini">
        
